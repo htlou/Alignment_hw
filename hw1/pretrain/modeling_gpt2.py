deebd40ed19d72bbfc291a71dcfef3d906e779ce
htlou <lht_pku@stu.pku.edu.cn>
2025-01-10 08:31:43 +0000

    [2200017789] Apply Weight Init

--- hw1/pretrain/modeling_gpt2.py
+++ hw1/pretrain/modeling_gpt2.py
@@ -88,6 +88,17 @@ class GPT(nn.Module):
         ))
         # Final classifier
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False)
+        self.transformer.wte.weight = self.lm_head.weight
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets = None):
         B, T = idx.size()
