409b59832db9f996767d88a51b8917ac22b8c4ef
htlou <lht_pku@stu.pku.edu.cn>
2025-01-10 11:36:48 +0000

    Generation Results

---
+++ hw1/finetune/batch_generate.sh
@@ -0,0 +1,22 @@
+MODEL_DIRS=(
+    "/data/align-anything/hantao/models/gpt2 gpt2-original"
+    "/data/align-anything/hantao/Alignment_hw/hw1/finetune/results/outputs/gpt2-alpaca-lora-rank-0-20250110-094906 gpt2-full-parameter"
+    "/data/align-anything/hantao/Alignment_hw/hw1/finetune/results/outputs/gpt2-alpaca-lora-rank-1-20250110-094908 gpt2-lora-rank-1"
+    "/data/align-anything/hantao/Alignment_hw/hw1/finetune/results/outputs/gpt2-alpaca-lora-rank-2-20250110-094906 gpt2-lora-rank-2"
+    "/data/align-anything/hantao/Alignment_hw/hw1/finetune/results/outputs/gpt2-alpaca-lora-rank-4-20250110-094907 gpt2-lora-rank-4"
+    "/data/align-anything/hantao/Alignment_hw/hw1/finetune/results/outputs/gpt2-alpaca-lora-rank-8-20250110-094908 gpt2-lora-rank-8"
+    "/data/align-anything/hantao/Alignment_hw/hw1/finetune/results/outputs/gpt2-alpaca-lora-rank-16-20250110-094908 gpt2-lora-rank-16"
+    "/data/align-anything/hantao/Alignment_hw/hw1/finetune/results/outputs/gpt2-alpaca-lora-rank-32-20250110-094902 gpt2-lora-rank-32"
+)
+
+LORA_RANK=(0 0 1 2 4 8 16 32)
+GPU_IDS=(0 1 2 3 4 5 6 7)
+for i in {0..7}; do
+    model=${MODEL_DIRS[$i]}
+    model_name=${model[1]}
+    model_dir=${model[0]}
+    echo "Generating for ${model_name}..."
+    CUDA_VISIBLE_DEVICES=${GPU_IDS[$i]} bash generate.sh ${model_dir} ${model_name} ${LORA_RANK[$i]} &
+done
+
+wait
--- hw1/finetune/generate.sh
+++ hw1/finetune/generate.sh
@@ -1,12 +1,16 @@
+MODEL_DIR=$1
+MODEL_NAME=$2
+LORA_RANK=$3
+
 python generate.py \
-    --model_name_or_path gpt2 \
+    --model_name_or_path ${MODEL_DIR} \
     --max_length 512 \
     --trust_remote_code True \
     --use_lora False \
-    --lora_dim 8 \
+    --lora_dim ${LORA_RANK} \
     --lora_scaling 32 \
     --lora_module_name h. \
-    --lora_load_path path-to-lora-checkpoint \
+    --lora_load_path ${MODEL_DIR} \
     --seed 42 \
-    --use_cuda False \
-    --output_dir_name gpt2-alpaca-eval
+    --use_cuda True \
+    --output_dir_name ${MODEL_NAME}-eval
