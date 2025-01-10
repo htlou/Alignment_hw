a8dba501b5ce11a951c1ab6dcbd9a14452a38558
htlou <lht_pku@stu.pku.edu.cn>
2025-01-10 08:05:10 +0000

    [2200017789] From Pretrained

--- hw1/pretrain/modeling_gpt2.py
+++ hw1/pretrain/modeling_gpt2.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from transformers import GPT2LMHeadModel
 
 
 @dataclass
@@ -11,6 +12,7 @@ class GPTConfig:
     n_layer : int = 12
     n_head : int = 12
     n_embd : int = 768
+    bias: bool = False
 
 class CausalSelfAttention(nn.Moudule):
     def __init__(self, config):
@@ -45,7 +47,7 @@ class CausalSelfAttention(nn.Moudule):
 class MLP(nn.Module):
     
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
@@ -76,7 +78,7 @@ class GPT(nn.Module):
             # Token Embedding
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             # Position Embedding
-            wpe = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
             # Hidden layer
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
             # Layer norm
@@ -84,3 +86,48 @@ class GPT(nn.Module):
         ))
         # Final classifier
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False)
+
+     @classmethod
+    def from_pretrained(cls, model_type, override_args=None):
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        override_args = override_args or {} # default to empty dict
+        # only dropout can be overridden see more notes below
+        assert all(k == 'dropout' for k in override_args)
+        print("loading weights from pretrained gpt: %s" % model_type)
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024),
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), 
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        config_args['vocab_size'] = 50257 
+        config_args['block_size'] = 1024 
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
+        
+        # init hf module
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+        # copy paramether
+        sd_keys_hf = sd_hf.keys()
+        
+        # buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # Treatment for Conv1D
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla for other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
